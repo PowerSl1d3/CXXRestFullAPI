7bc9e54126d8cc22249cafb18b9c7dcd0c3dfc1c
PowerSl1d3 <manikryte@gmail.com>
2021-10-18 23:15:17 -0700

    [#3] Add file storage

--- lib/userDataBase.cpp
+++ lib/userDataBase.cpp
@@ -4,35 +4,36 @@
 
 #include "userDataBase.h"
 
-userDataBase::userDataBase(std::shared_ptr<sql::Statement> stmt_) :
-stmt(stmt_)
+userDataBase::userDataBase(const std::shared_ptr<sql::Connection> con) :
+        con_(con),
+        stmt_(con->createStatement())
 {
-    stmt->execute("USE mysql");
-    stmt->execute("DROP TABLE IF EXISTS todoUser");
-    stmt->execute("CREATE TABLE todoUser(id INT PRIMARY KEY AUTO_INCREMENT,"
+    stmt_->execute("USE mysql");
+    stmt_->execute("DROP TABLE IF EXISTS todoUser");
+    stmt_->execute("CREATE TABLE todoUser(id INT PRIMARY KEY AUTO_INCREMENT,"
                   " username VARCHAR(30), password VARCHAR(256));");
 }
 
 bool userDataBase::checkUserAlreadyExist(const std::string& username, const std::string& password) {
-    return stmt->executeQuery("SELECT * FROM todoUser WHERE username = '" + username +
-    "' AND password = '" + password + "'")->rowsCount();
+    const std::string statement = "SELECT * FROM todoUser WHERE username = ? AND password = ?";
+    preparedStatement_.reset(con_->prepareStatement(statement));
+    preparedStatement_->setString(1, username);
+    preparedStatement_->setString(2, password);
+    return preparedStatement_->executeQuery()->rowsCount();
 }
 
 void userDataBase::createUser(const std::string& username, const std::string& password) {
-    stmt->execute("INSERT INTO todoUser(username, password)"
-                  " VALUES ('"
-                  + username +
-                  "', '"
-                  + password +
-                  "')");
+    const std::string statement = "INSERT INTO todoUser(username, password) VALUES (?, ?)";
+    preparedStatement_.reset(con_->prepareStatement(statement));
+    preparedStatement_->setString(1, username);
+    preparedStatement_->setString(2, password);
+    preparedStatement_->execute();
 }
 
 std::unique_ptr<sql::ResultSet> userDataBase::getUserId(const std::string& username, const std::string& password) {
-    return std::unique_ptr<sql::ResultSet>(
-    stmt->executeQuery("SELECT id FROM todoUser WHERE username = '"
-                              + username +
-                              "' AND password = '"
-                              + password +
-                              "'")
-    );
+    const std::string statement = "SELECT id FROM todoUser WHERE username = ? AND password = ?";
+    preparedStatement_.reset(con_->prepareStatement(statement));
+    preparedStatement_->setString(1, username);
+    preparedStatement_->setString(2, password);
+    return std::unique_ptr<sql::ResultSet>(preparedStatement_->executeQuery());
 }
