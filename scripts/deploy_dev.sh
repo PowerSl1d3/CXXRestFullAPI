1d7d6e7b61e7fc899e6983cc23087dcdea0196c7
PowerSl1d3 <manikryte@gmail.com>
2021-12-04 22:40:14 -0800

    [#9] Add git DnD

---
+++ scripts/deploy_dev.sh
@@ -0,0 +1,2 @@
+git push origin develop
+ssh root@164.90.163.58 "cd /root/server/CXXRestFullAPI && git pull && git checkout develop && scripts/build.sh"
---
+++ scripts/deploy_master.sh
@@ -0,0 +1,2 @@
+git push origin master
+ssh root@164.90.163.58 "cd /root/server/CXXRestFullAPI && git pull && git checkout master && scripts/build.sh"
